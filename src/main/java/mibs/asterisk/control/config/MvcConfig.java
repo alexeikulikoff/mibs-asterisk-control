8ae4bf01dc0ca1fe33b55b4ba58303ef6efbab87
alexeikulikoff <alexei.kulikoff@gmail.com>
2018-09-22 17:12:53 +0300

    add redis support

--- src/main/java/mibs/asterisk/control/config/MvcConfig.java
+++ src/main/java/mibs/asterisk/control/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package mibs.asterisk.control.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,8 +12,9 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
         registry.addViewController("/login-error").setViewName("login-error");
+        registry.addViewController("/login").setViewName("login");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 
 }
--- src/main/java/mibs/asterisk/control/config/WebSecurityConfig.java
+++ src/main/java/mibs/asterisk/control/config/WebSecurityConfig.java
@@ -6,17 +6,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import mibs.asterisk.control.service.UsersDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 600)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+    	http
             .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
+                .antMatchers("/restore").permitAll()
+   			 	.antMatchers("/forgot_password").permitAll()
+   			 	.antMatchers("/verifyMail").permitAll()
+   			 	.antMatchers("/css/**").permitAll().anyRequest().permitAll()
+   			 	.antMatchers("/font-awesome/**").permitAll().anyRequest().permitAll()
+   			 	.antMatchers("/js/**").permitAll().anyRequest().permitAll()   			 	
+   			 	.antMatchers("/img/**").permitAll()
+   			 	.antMatchers("/fragments/**").permitAll()
+   			 	.antMatchers("/admin/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
