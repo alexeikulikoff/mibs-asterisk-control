ec40868339aebab579bf64a4e325f723cbfb6765
alexeikulikoff <alexei.kulikoff@gmail.com>
2019-03-06 12:42:30 +0300

    set session timeout 2

--- bin/main/static/js/dist/queues.js
+++ bin/main/static/js/dist/queues.js
@@ -4358,9 +4358,11 @@ queues.createTable = function( dataSet ){
 			data: dataSet.records,
 		 	columns:
 		 	  [
-			    { title : $label.date,  data : "date"},
+		 		{ title : $label.id,  data : "id"},
+		 		{ title : $label.date,  data : "date"},
 				{ title : $label.enter,  data : "enterTime"},
 		 		{ title : $label.exit,  data : "exitTime"},
+		 		{ title : $label.duration,  data : "duration"},
 		 		{ title : $label.phone,  data : "peer"},
 		 		{ title : $label.calls,  data : "calls"},
 		 		{ title : $label.show,  data : "date", render : function(data, type, row ){
@@ -4605,6 +4607,9 @@ queues.showQueueSpell = function( page ){
 				  success: function( report ){
 					
 					  queues.createTable( report );
+					  
+					  $("#total-duration").text(report.totalduration);
+					  $("#abel.totalcall").text(report.totalcall);
 					
 					  $("#queue-report-container").removeClass("hidden");
 					  
